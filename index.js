62608f64371ad28c0d5ecc44efef060c5927f06d
Carl Svensson <csvensson@apple.com>
2024-12-18 21:21:08 -0800

    Add support for star chart requests

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const { PositionRequest, ObserverParameters } = require('./src/request_objects');
+const { format } = require('date-fns');
+const { PositionRequest, ObserverParameters, EventRequest, BodiesEnumRequest, StarChartRequest, ViewParameters } = require('./src/request_objects');
 
 async function main() {
+  const today = new Date();
+  const nextMonth = new Date(today.setMonth(today.getMonth() + 1));
+  
+  console.log(nextMonth.toISOString().split('T')[0]); // Output: YYYY-MM-DD
+  
   // Make a test request
   pr = new PositionRequest();
-  await pr.fetch(new ObserverParameters());
-  console.log(`Response Data: ${pr.data}`)
+  op = new ObserverParameters({to_date: format(nextMonth, 'yyyy-MM-dd')});
+  await pr.fetch(op);
+
+  er = new EventRequest();
+  await er.fetch(op, "sun");
+
+  br = new BodiesEnumRequest();
+  await br.fetch(op);
+
+  sr = new StarChartRequest();
+  sr_params = {
+    observer: op,
+    view: new ViewParameters(),
+  }
+  await sr.fetch(sr_params);
 }
 
 main();
--- src/request_objects.js
+++ src/request_objects.js
@@ -8,19 +8,38 @@ class ObserverParameters {
   constructor(args = {}) {
     const today = new Date();
     
-    this.latitude = args.latitude || process.env.DEFAULT_LATITUDE;
-    this.longitude = args.longitude || process.env.DEFAULT_LONGITUDE;
+    this.latitude = parseFloat(args.latitude || process.env.DEFAULT_LATITUDE);
+    this.longitude = parseFloat(args.longitude || process.env.DEFAULT_LONGITUDE);
     this.elevation = args.elevation || process.env.DEFAULT_ELEVATION;
     this.from_date = args.from_date || format(today, 'yyyy-MM-dd');
-    this.to_date = args.from_date || format(today, 'yyyy-MM-dd');
+    this.to_date = args.to_date || format(today, 'yyyy-MM-dd');
     this.time = args.time || format(today, 'HH:mm:ss');
     this.output = 'rows';
+    this.date = args.date || format(today, 'yyyy-MM-dd');
+  }
+}
+
+class ViewParameters {
+  constructor(args = {}) {
+    this.type = args.type || 'constellation';
+    this.parameters = {
+      constellation: args.constellation || 'ori',
+      position: {
+        equatorial: {
+          rightAscension: parseFloat(args.rightAscension || 0),
+          declination: parseFloat(args.declination || 0),
+        },
+        zoom: args.zoom || 3
+      }
+    };
   }
 }
 
 class BaseRequest {
   data = null;
   response = null;
+  url = null;
+  method = 'get';
   
   constructor(appId = null, appSecret = null) {
     if(appId == null || appSecret == null) {
@@ -29,18 +48,11 @@ class BaseRequest {
       this.header = auth_header(appId, appSecret);
     }
   }
-}
-
-class PositionRequest extends BaseRequest {
-  url = "/bodies/positions";
-  
-  constructor(appId = null, appSecret = null) {
-    super(appId, appSecret);
-  }
   
   // Make a request using the given parameters
-  async fetch(observer_parameters) {
-    const request_url = `${APIBASE}${this.url}`;
+  async fetch(parameters, body = null) {
+    var request_url = `${APIBASE}${this.url}`;
+    request_url = (body != null) ? `${request_url}/${body}` : request_url
     
     // Add a request interceptor for debug
     // axios.interceptors.request.use(request => {
@@ -49,10 +61,17 @@ class PositionRequest extends BaseRequest {
     // });
     
     try {
-      var resp = await axios.get(request_url, {
-        headers: this.header,
-        params: observer_parameters
-      });
+      var resp;
+      if(this.method == 'get') {
+        resp = await axios.get(request_url, {
+          headers: this.header,
+          params: parameters
+        });
+      } else if(this.method == 'post') {
+        resp = await axios.post(request_url, parameters, {headers: this.header});
+      } else {
+        return;
+      }
       
       this.response = resp;
       
@@ -60,10 +79,39 @@ class PositionRequest extends BaseRequest {
       console.dir(resp.data.data, {depth: 8, colors: true})
       
     } catch (error) {
-      console.log(`Could not complete request to ${request_url} with parameters ${JSON.stringify(observer_parameters)} and header ${this.header}: ${error}`);
+      console.log(`Could not complete request to ${request_url} with parameters ${JSON.stringify(parameters)} and header ${this.header}: ${error}`);
       console.dir(error, {depth: 6, colors: true})
     }
   }
 }
 
-module.exports = { ObserverParameters, BaseRequest, PositionRequest }
+class PositionRequest extends BaseRequest {
+  constructor(appId = null, appSecret = null) {
+    super(appId, appSecret);
+    this.url = "/bodies/positions";
+  }
+}
+
+class EventRequest extends BaseRequest {
+  constructor(appId = null, appSecret = null) {
+    super(appId, appSecret);
+    this.url = "/bodies/events";
+  }
+}
+
+class BodiesEnumRequest extends BaseRequest {
+  constructor(appId = null, appSecret = null) {
+    super(appId, appSecret);
+    this.url = "/bodies";
+  }
+}
+
+class StarChartRequest extends BaseRequest {
+  constructor(appId = null, appSecret = null) {
+    super(appId, appSecret);
+    this.url = "/studio/star-chart";
+    this.method = 'post';
+  }
+}
+
+module.exports = { ObserverParameters, BaseRequest, PositionRequest, EventRequest, BodiesEnumRequest, ViewParameters, StarChartRequest }
